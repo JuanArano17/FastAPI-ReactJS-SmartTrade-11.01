4fa22d79a6822a5ec0331449bf3ae4441df1a705
JuanArano17 <juanarano17@gmail.com>
2024-02-22 11:00:39 +0100

    Route creation

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import EPGetAll from './components/EPGetAll';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <EPGetAll/>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <EPGetAll/>
+          </Route>
+          <Route exact path="/">
+            <EPGetAll/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
