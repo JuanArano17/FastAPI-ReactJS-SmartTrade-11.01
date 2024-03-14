e6a295ac80c1b4013a75f3b119b322564c875f2b
Rubo0000 <rubenarranz@hotmail.com>
2024-03-14 11:47:20 +0100

    Cambio de rama del Login

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import RegistrationForm from './pages/RegisterPage';
 import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route path="/register" component={RegisterPage} />
-          <Route path="/login" component={LoginPage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
       </Router>
