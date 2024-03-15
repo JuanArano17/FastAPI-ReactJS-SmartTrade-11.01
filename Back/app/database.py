b57356091c82db309d575d5ee6549807ff8139c2
Lucasarl <90321541+Lucasarl@users.noreply.github.com>
2024-03-15 14:02:55 +0100

    feat: adding imports of new models to database.py

--- Back/app/database.py
+++ Back/app/database.py
@@ -9,6 +9,14 @@ from models.product_seller import Base as ProductSellerBase
 from models.card import Base as CardBase
 from models.product_line import Base as ProductLineBase
 from models.refund_product import Base as RefundProductBase
+from models.buyer_address import Base as BuyerAddressBase
+from models.category import Base as CategoryBase
+from models.image import Base as ImageBase
+from models.in_shopping_cart import Base as InShoppingCartBase
+from models.in_wish_list import Base as InWishListBase
+from models.product import Base as ProductBase
+from models.seller import Base as SellerBase
+from models.buyer import Base as BuyerBase
 
 
 def get_engine(user, passwd, host, port, db):
@@ -42,4 +50,13 @@ def get_session():
     ProductLineBase.metadata.create_all(bind=engine)
     ProductSellerBase.metadata.create_all(bind=engine)
     RefundProductBase.metadata.create_all(bind=engine)
+    ProductBase.metadata.create_all(bind=engine)
+    SellerBase.metadata.create_all(bind=engine)
+    BuyerBase.metadata.create_all(bind=engine)
+    ImageBase.metadata.create_all(bind=engine)
+    InShoppingCartBase.metadata.create_all(bind=engine)
+    InWishListBase.metadata.create_all(bind=engine)
+    CategoryBase.metadata.create_all(bind=engine)
+    BuyerAddressBase.metadata.create_all(bind=engine)
+
     return session
