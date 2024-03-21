e57a48dd91eedbe55e9eb721f5ecffde53015386
VoperAD <victoraguiarrodrigues@yahoo.com>
2024-03-21 13:50:58 +0100

    feat: alembic migrations setup completed

--- Back/app/database.py
+++ Back/app/database.py
@@ -24,11 +24,15 @@ from app.models.refund_product import RefundProduct
 load_dotenv()
 
 
-def get_engine() -> Engine:
-    url = os.getenv(
+def get_db_url() -> str:
+    return os.getenv(
         "DB_URL",
         "postgresql+psycopg://postgres:password@localhost:5432/database",
     )
+
+
+def get_engine() -> Engine:
+    url = get_db_url()
     if not database_exists(url):
         create_database(url)
     engine = create_engine(url, pool_size=50, echo=True)
