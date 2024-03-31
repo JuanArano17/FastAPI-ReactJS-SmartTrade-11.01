6a699acc3d3568919ba965077dbc922dc087fd6b
Lucasarl <90321541+Lucasarl@users.noreply.github.com>
2024-03-31 12:08:45 +0200

    feat: buyer and seller api completed

--- Back/app/api/main.py
+++ Back/app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from app.api.routers import buyers
 from app.api.routers import sellers
 
 api_router = APIRouter()
 api_router.include_router(sellers.router)
+api_router.include_router(buyers.router)
---
+++ Back/app/api/routers/buyers.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter
+
+from app.schemas.buyer import Buyer, BuyerCreate, BuyerUpdate
+from database import get_session
+from service.buyer import BuyerService
+from service.user import UserService
+
+router = APIRouter(prefix="/buyers", tags=["buyers"])
+
+session=get_session()
+user_service=UserService(session=session)
+buyer_service=BuyerService(session=session,user_service=user_service)
+
+@router.get("/", response_model=list[Buyer])
+async def read_buyers():
+    """
+    Retrieve buyers.
+    """
+    return buyer_service.get_all()
+
+
+@router.get("/{buyer_id}", response_model=Buyer)
+async def read_buyer(*, buyer_id: int):
+    """
+    Retrieve a buyer.
+    """
+    return buyer_service.get_by_id(buyer_id)
+
+
+@router.post("/", response_model=Buyer)
+async def create_buyer(*, buyer: BuyerCreate):
+    """
+    Create a new buyer.
+    """
+    return buyer_service.add(buyer)
+
+
+@router.put("/{buyer_id}", response_model=Buyer)
+async def update_buyer(*, buyer_id: int, buyer: BuyerUpdate):
+    """
+    Update a buyer.
+    """
+    return buyer_service.update(buyer_id,buyer)
+
+
+@router.delete("/{buyer_id}")
+async def delete_buyer(*, buyer_id: int):
+    """
+    Delete a buyer.
+    """
+    return buyer_service.delete_by_id(buyer_id)
+
+
+@router.delete("/")
+async def delete_buyers():
+    """
+    Delete all buyers.
+    """
+    return buyer_service.delete_all()
