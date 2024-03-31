e574bf6b3ddb861f8188d31520d982cd27d31a01
Lucasarl <90321541+Lucasarl@users.noreply.github.com>
2024-03-31 15:35:06 +0200

    feat: adding product endpoints

--- Back/app/api/main.py
+++ Back/app/api/main.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.routers import buyers
 from app.api.routers import sellers
+from app.api.routers import products
 
 api_router = APIRouter()
 api_router.include_router(sellers.router)
 api_router.include_router(buyers.router)
+api_router.include_router(products.router)
