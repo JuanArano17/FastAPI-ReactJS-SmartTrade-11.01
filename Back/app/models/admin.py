3bb06fc00b3b5e2109ad4b4e58323bb777c5507d
VoperAD <victoraguiarrodrigues@yahoo.com>
2024-04-03 21:14:49 +0200

    feat: add Admin model

---
+++ Back/app/models/admin.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from app.models.user import User
+
+
+class Admin(User):
+    __tablename__ = "Admin"
+
+    id = Column(
+        Integer,
+        ForeignKey("User.id", ondelete="CASCADE", name="fk_admin_user_id"),
+        primary_key=True,
+        index=True,
+    )
+
+    __mapper_args__ = {
+        "polymorphic_identity": "Admin",
+    }
+
+    def __repr__(self):
+        return f"Admin(id={self.id}, email='{self.email}', name='{self.name}', surname='{self.surname}', password='{self.password}')"
