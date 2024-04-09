4fac132c322d61af9893930da1fd5c90142476ab
JuanArano17 <juanarano17@gmail.com>
2024-04-09 15:40:01 +0200

    a

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import RegisterSelectionPage from './pages/RegisterSelectionPage';
-import RegisterSellerPage from './pages/RegisterSellerPage';
-import RegisterBuyerPage from './pages/RegisterBuyerPage';
 import LoginPage from './pages/LoginPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import ProductDetailPage from './components/ProductDetailPage';
@@ -17,12 +15,10 @@ function App() {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route path="/login" component={LoginPage} />
-          <Route path="/Details" component={ProductDetailPage} />
-          <Route path="/Catalog" component={CatalogPage} />
+          <Route path="/details" component={ProductDetailPage} />
+          <Route path="/catalog" component={CatalogPage} />
           <Route path="/forgotPassword" component={ForgotPasswordPage} />
           <Route path="/register" component={RegisterSelectionPage} />
-          <Route path="/registerSeller" component={RegisterSellerPage} />
-          <Route path="/registerBuyer" component={RegisterBuyerPage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
       </Router>
