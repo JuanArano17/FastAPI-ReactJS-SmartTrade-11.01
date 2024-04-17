8f05d47bc38d5fdca527f0bb88d4b0fd97534f98
VoperAD <victoraguiarrodrigues@yahoo.com>
2024-04-17 11:17:51 +0200

    fix: use selectin polymorphic on get all

--- Back/app/service/product.py
+++ Back/app/service/product.py
@@ -1,8 +1,8 @@
-from typing import Union
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectin_polymorphic
 from fastapi import HTTPException, status
 from sqlalchemy.exc import ProgrammingError
+from sqlalchemy import select
 
 from app.models.product import Product
 from app.crud_repository import CRUDRepository
@@ -56,20 +56,24 @@ class ProductService:
             detail=f"Product with id {product_id} not found.",
         )
 
-    def get_all(self):
-        return self.product_factory.get_all()
+    def get_all(self) -> list[Product]:
+        load_opt = selectin_polymorphic(
+            Product,
+            [Book, Game, Electronics, Clothes, Food, HouseUtilities, Electrodomestics],
+        )
+        return self.session.scalars(select(Product).options(load_opt)).all()  # type: ignore
 
     def update(self, product_id, new_data: dict):
         return self.product_factory.update_product(product_id, new_data)
 
     def delete_by_id(self, product_id):
         # self.product_repo.delete_by_id(product_id)
-        product=self.get_by_id(product_id)
+        product = self.get_by_id(product_id)
         if not product:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail=f"Product with id {product_id} not found.",
-                )
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {product_id} not found.",
+            )
         self.product_repo.delete_by_id(id=product_id)
 
     def delete_all(self):
@@ -82,46 +86,49 @@ class ProductFactory:
 
     def create_product(self, category: str, product: dict):
         try:
-            if category.lower() == 'book':
-                book=BookCreate(**product)
+            if category.lower() == "book":
+                book = BookCreate(**product)
                 return self.service.book_repo.add(Book(**book.model_dump()))
-            elif category.lower() == 'game':
-                game=GameCreate(**product)
+            elif category.lower() == "game":
+                game = GameCreate(**product)
                 return self.service.game_repo.add(Game(**game.model_dump()))
-            elif category.lower() == 'clothes':
-                clothes=ClothesCreate(**product)
+            elif category.lower() == "clothes":
+                clothes = ClothesCreate(**product)
                 return self.service.clothes_repo.add(Clothes(**clothes.model_dump()))
-            elif category.lower() == 'electronics':
-                electronics=ElectronicsCreate(**product)
-                return self.service.electronics_repo.add(Electronics(**electronics.model_dump()))
-            elif category.lower() == 'houseutilities':
-                house_utilities=HouseUtilitiesCreate(**product)
-                return self.service.house_utilities_repo.add(HouseUtilities(**house_utilities.model_dump()))
-            elif category.lower() == 'food':
-                food=FoodCreate(**product)
+            elif category.lower() == "electronics":
+                electronics = ElectronicsCreate(**product)
+                return self.service.electronics_repo.add(
+                    Electronics(**electronics.model_dump())
+                )
+            elif category.lower() == "houseutilities":
+                house_utilities = HouseUtilitiesCreate(**product)
+                return self.service.house_utilities_repo.add(
+                    HouseUtilities(**house_utilities.model_dump())
+                )
+            elif category.lower() == "food":
+                food = FoodCreate(**product)
                 return self.service.food_repo.add(Food(**food.model_dump()))
-            elif category.lower() == 'electrodomestics':
-                electrodomestics=ElectrodomesticsCreate(**product)
-                return self.service.electrodomestics_repo.add(Electrodomestics(**electrodomestics.model_dump()))
+            elif category.lower() == "electrodomestics":
+                electrodomestics = ElectrodomesticsCreate(**product)
+                return self.service.electrodomestics_repo.add(
+                    Electrodomestics(**electrodomestics.model_dump())
+                )
             else:
                 raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail="Invalid product category"
-            )
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail="Invalid product category",
+                )
         except ValidationError as e:
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=str(e)
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
             )
         except ProgrammingError as s:
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=str(s)
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(s)
             )
-        
-    def update_product(self, product_id:int, new_data:dict):
-        
-        product=self.service.get_by_id(product_id)
+
+    def update_product(self, product_id: int, new_data: dict):
+        product = self.service.get_by_id(product_id)
         # Extract common attributes from update_data
         # Extract common attributes from new_data
         common_attributes = {}
@@ -129,52 +136,73 @@ class ProductFactory:
             if key in new_data:
                 common_attributes[key] = new_data.pop(key)
         try:
-            if(len(common_attributes)!=0):
-                product_update=ProductUpdate(**common_attributes)
-                product=self.service.product_repo.update(product,product_update)
-        
-            if(len(new_data)==0):
+            if len(common_attributes) != 0:
+                product_update = ProductUpdate(**common_attributes)
+                product = self.service.product_repo.update(product, product_update)
+
+            if len(new_data) == 0:
                 return product
-        
-            if product.category.lower() == 'book':
-                new_book_data=BookUpdate(**new_data)
+
+            if product.category.lower() == "book":
+                new_book_data = BookUpdate(**new_data)
                 return self.service.book_repo.update(product, new_book_data)
-            elif product.category.lower() == 'game':
-                new_game_data=GameUpdate(**new_data)
+            elif product.category.lower() == "game":
+                new_game_data = GameUpdate(**new_data)
                 return self.service.game_repo.update(product, new_game_data)
-            elif product.category.lower() == 'clothes':
-                new_clothes_data=ClothesUpdate(**new_data)
+            elif product.category.lower() == "clothes":
+                new_clothes_data = ClothesUpdate(**new_data)
                 return self.service.clothes_repo.update(product, new_clothes_data)
-            elif product.category.lower() == 'electronics':
-                new_electronics_data=ElectronicsUpdate(**new_data)
-                return self.service.electronics_repo.update(product, new_electronics_data)
-            elif product.category.lower() == 'houseutilities':
-                new_house_utilities_data=HouseUtilitiesUpdate(**new_data)
-                return self.service.house_utilities_repo.update(product, new_house_utilities_data)
-            elif product.category.lower() == 'food':
-                new_food_data=FoodUpdate(**new_data)
+            elif product.category.lower() == "electronics":
+                new_electronics_data = ElectronicsUpdate(**new_data)
+                return self.service.electronics_repo.update(
+                    product, new_electronics_data
+                )
+            elif product.category.lower() == "houseutilities":
+                new_house_utilities_data = HouseUtilitiesUpdate(**new_data)
+                return self.service.house_utilities_repo.update(
+                    product, new_house_utilities_data
+                )
+            elif product.category.lower() == "food":
+                new_food_data = FoodUpdate(**new_data)
                 return self.service.food_repo.update(product, new_food_data)
-            elif product.category.lower() == 'electrodomestics':
-                new_electrodomestics_data=ElectrodomesticsUpdate(**new_data)
-                return self.service.electrodomestics_repo.update(product, new_electrodomestics_data)
+            elif product.category.lower() == "electrodomestics":
+                new_electrodomestics_data = ElectrodomesticsUpdate(**new_data)
+                return self.service.electrodomestics_repo.update(
+                    product, new_electrodomestics_data
+                )
         except ValidationError as e:
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=str(e)
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)
             )
         except ProgrammingError as s:
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=str(s)
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(s)
             )
 
     def get_all(self):
-        products=[]
-        products+=self.service.game_repo.get_all()
-        products+=self.service.book_repo.get_all()
-        products+=self.service.clothes_repo.get_all()
-        products+=self.service.electronics_repo.get_all()
-        products+=self.service.house_utilities_repo.get_all()
-        products+=self.service.food_repo.get_all()
-        products+=self.service.electrodomestics_repo.get_all()
+        products = []
+        games: list[Game] = []
+        books: list[Book] = []
+        foods: list[Food] = []
+        electronics: list[Electronics] = []
+        electrodomestics: list[Electrodomestics] = []
+        clothes: list[Clothes] = []
+        house_utilities: list[HouseUtilities] = []
+
+        games += self.service.game_repo.get_all()
+        books += self.service.book_repo.get_all()
+        clothes += self.service.clothes_repo.get_all()
+        electronics += self.service.electronics_repo.get_all()
+        house_utilities += self.service.house_utilities_repo.get_all()
+        foods += self.service.food_repo.get_all()
+        electrodomestics += self.service.electrodomestics_repo.get_all()
+        products = {
+            "games": games,
+            "books": books,
+            "clothes": clothes,
+            "electronics": electronics,
+            "house_utilities": house_utilities,
+            "foods": foods,
+            "electrodomestics": electrodomestics,
+        }
         return products
