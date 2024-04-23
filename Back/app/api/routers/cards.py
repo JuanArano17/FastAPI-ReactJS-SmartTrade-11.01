ce644908f848b36bea709e14558f650fb1175975
VoperAD <victoraguiarrodrigues@yahoo.com>
2024-04-23 22:35:37 +0200

    refactor: use JWT on Cards endpoints

--- Back/app/api/routers/cards.py
+++ Back/app/api/routers/cards.py
@@ -1,76 +1,140 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
-from app.api.deps import CardServiceDep
+from app.api.deps import CardServiceDep, CurrentUserDep
 from app.schemas.users.card import Card, CardCreate, CardUpdate
 
-router = APIRouter(prefix="/buyers/{buyer_id}/cards", tags=["cards"])
+# router = APIRouter(prefix="/buyers/{buyer_id}/cards", tags=["Cards"])
 
 
-@router.get("/", response_model=list[Card])
-async def read_cards(*, buyer_id: int, card_service: CardServiceDep):
+# @router.get("/", response_model=list[Card])
+# async def read_cards(*, buyer_id: int, card_service: CardServiceDep):
+#     """
+#     Retrieve cards from buyer.
+#     """
+#     return card_service.get_by_id_buyer(id_buyer=buyer_id)
+
+
+# @router.post("/", response_model=Card)
+# async def create_card(*, buyer_id: int, card: CardCreate, card_service: CardServiceDep):
+#     """
+#     Create a new card for the buyer.
+#     """
+#     return card_service.add(id_buyer=buyer_id, card=card)
+
+
+# @router.delete("/")
+# async def delete_cards(buyer_id: int, card_service: CardServiceDep):
+#     """
+#     Delete all cards from a buyer.
+#     """
+#     return card_service.delete_all_by_id_buyer(id_buyer=buyer_id)
+
+
+# @router.get("/{card_id}", response_model=Card)
+# async def read_card(*, buyer_id: int, card_id: int, card_service: CardServiceDep):
+#     """
+#     Retrieve a specific buyer card.
+#     """
+
+#     card = card_service.get_by_id(card_id)
+
+#     if card is None or card.id_buyer != buyer_id:
+#         raise HTTPException(status_code=404, detail="Card not found")
+
+#     return card
+
+
+# @router.put("/{card_id}", response_model=Card)
+# async def update_card(
+#     *,
+#     buyer_id=int,
+#     card_id: int,
+#     card: CardUpdate,
+#     card_service: CardServiceDep,
+# ):
+#     """
+#     Update a card.
+#     """
+#     existing_card = card_service.get_by_id(card_id)
+
+#     if existing_card is None or existing_card.id_buyer != int(buyer_id):
+#         raise HTTPException(status_code=404, detail="Card not found")
+
+#     return card_service.update(card_id=card_id, new_data=card)
+
+
+# @router.delete("/{card_id}")
+# async def delete_card(*, buyer_id=int, card_id: int, card_service: CardServiceDep):
+#     """
+#     Delete a card.
+#     """
+#     existing_card = card_service.get_by_id(card_id)
+
+#     if existing_card is None or existing_card.id_buyer != int(buyer_id):
+#         raise HTTPException(status_code=404, detail="Card not found")
+
+#     return card_service.delete_by_id(card_id)
+
+
+cards_router = APIRouter(prefix="/cards/me", tags=["Cards"])
+
+
+@cards_router.get("/", response_model=list[Card])
+async def read_my_cards(current_user: CurrentUserDep, card_service: CardServiceDep):
     """
-    Retrieve cards from buyer.
+    Retrieve cards from the current user.
     """
-    return card_service.get_by_id_buyer(id_buyer=buyer_id)
+    return card_service.get_by_user(current_user)
 
 
-@router.post("/", response_model=Card)
-async def create_card(*, buyer_id: int, card: CardCreate, card_service: CardServiceDep):
+@cards_router.get("/{card_id}", response_model=Card)
+async def read_my_card(
+    card_id: int, current_user: CurrentUserDep, card_service: CardServiceDep
+):
     """
-    Create a new card for the buyer.
+    Retrieve a specific card from the current user.
     """
-    return card_service.add(id_buyer=buyer_id, card=card)
+    return card_service.get_one_by_user(current_user, card_id)
 
 
-@router.delete("/")
-async def delete_cards(buyer_id: int, card_service: CardServiceDep):
+@cards_router.delete("/")
+async def delete_my_cards(current_user: CurrentUserDep, card_service: CardServiceDep):
     """
-    Delete all cards from a buyer.
+    Delete all cards from the current user.
     """
-    return card_service.delete_by_id_buyer(id_buyer=buyer_id)
+    card_service.delete_all_by_user(current_user)
 
 
-@router.get("/{card_id}", response_model=Card)
-async def read_card(*, buyer_id: int, card_id: int, card_service: CardServiceDep):
+@cards_router.post("/", response_model=Card)
+async def create_my_card(
+    card: CardCreate, current_user: CurrentUserDep, card_service: CardServiceDep
+):
     """
-    Retrieve a specific buyer card.
+    Create a new card for the current user.
     """
-
-    card = card_service.get_by_id(card_id)
-
-    if card is None or card.id_buyer != buyer_id:
-        raise HTTPException(status_code=404, detail="Card not found")
-
-    return card
+    return card_service.add_by_user(user=current_user, card=card)
 
 
-@router.put("/{card_id}", response_model=Card)
-async def update_card(
-    *,
-    buyer_id=int,
+@cards_router.put("/{card_id}", response_model=Card)
+async def update_my_card(
     card_id: int,
     card: CardUpdate,
+    current_user: CurrentUserDep,
     card_service: CardServiceDep,
 ):
     """
-    Update a card.
+    Update a card from the current user.
     """
-    existing_card = card_service.get_by_id(card_id)
-
-    if existing_card is None or existing_card.id_buyer != int(buyer_id):
-        raise HTTPException(status_code=404, detail="Card not found")
-
-    return card_service.update(card_id=card_id, new_data=card)
+    return card_service.update_by_user(
+        user=current_user, card_id=card_id, new_data=card
+    )
 
 
-@router.delete("/{card_id}")
-async def delete_card(*, buyer_id=int, card_id: int, card_service: CardServiceDep):
+@cards_router.delete("/{card_id}")
+async def delete_my_card(
+    card_id: int, current_user: CurrentUserDep, card_service: CardServiceDep
+):
     """
-    Delete a card.
+    Delete a card from the current user.
     """
-    existing_card = card_service.get_by_id(card_id)
-
-    if existing_card is None or existing_card.id_buyer != int(buyer_id):
-        raise HTTPException(status_code=404, detail="Card not found")
-
-    return card_service.delete_by_id(card_id)
+    return card_service.delete_one_by_user(card_id, current_user)
--- Back/app/service/users/card.py
+++ Back/app/service/users/card.py
@@ -1,3 +1,4 @@
+from app.models.users.types.user import User
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 
@@ -43,6 +44,10 @@ class CardService:
         self.card_repo.add(card_obj)
         return card_obj
 
+    def add_by_user(self, user: User, card: CardCreate) -> Card:
+        self._check_is_buyer(user)
+        return self.add(user.id, card)
+
     def get_by_id(self, id) -> Card:
         if card := self.card_repo.get_by_id(id):
             return card
@@ -51,6 +56,18 @@ class CardService:
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Card with id {id} not found.",
         )
+
+    def get_one_by_user(self, user: User, card_id) -> Card:
+        self._check_is_buyer(user)
+        self._check_buyer_owns_card(user.id, card_id)
+        return self.get_by_id(card_id)
+
+    def get_by_id_buyer(self, id_buyer) -> list[Card]:
+        return self.card_repo.get_by_id_buyer(id_buyer=id_buyer)
+
+    def get_by_user(self, user: User) -> list[Card]:
+        self._check_is_buyer(user)
+        return self.get_by_id_buyer(user.id)
 
     def get_all(self) -> list[Card]:
         return self.card_repo.get_all()
@@ -70,6 +87,11 @@ class CardService:
 
         return self.card_repo.update(card, new_data)
 
+    def update_by_user(self, user: User, card_id, new_data: CardUpdate) -> Card:
+        self._check_is_buyer(user)
+        self._check_buyer_owns_card(user.id, card_id)
+        return self.update(card_id, new_data)
+
     def delete_by_id(self, card_id):
         self.get_by_id(card_id)
         self.card_repo.delete_by_id(card_id)
@@ -77,8 +99,28 @@ class CardService:
     def delete_all(self):
         self.card_repo.delete_all()
 
-    def get_by_id_buyer(self, id_buyer) -> list[Card]:
-        return self.card_repo.get_by_id_buyer(id_buyer=id_buyer)
+    def delete_all_by_id_buyer(self, id_buyer):
+        self.card_repo.delete_by_id_buyer(id_buyer=id_buyer)
 
-    def delete_by_id_buyer(self, id_buyer) -> list[Card]:
-        return self.card_repo.delete_by_id_buyer(id_buyer=id_buyer)
+    def delete_all_by_user(self, user: User):
+        self._check_is_buyer(user)
+        self.delete_all_by_id_buyer(user.id)
+
+    def delete_one_by_user(self, card_id: int, user: User):
+        self._check_is_buyer(user)
+        self._check_buyer_owns_card(user.id, card_id)
+        self.delete_by_id(card_id)
+
+    def _check_is_buyer(self, user: User):
+        if not str(user.type) == "Buyer":
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Users of type {user.type} do not have cards.",
+            )
+
+    def _check_buyer_owns_card(self, buyer_id, card_id):
+        if not self.card_repo.get_where(Card.id == card_id, Card.id_buyer == buyer_id):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Card with id {card_id} not found for buyer with id {buyer_id}.",
+            )
