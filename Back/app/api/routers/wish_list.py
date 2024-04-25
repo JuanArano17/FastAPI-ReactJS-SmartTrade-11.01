61dcc1799a8d739f6ba669155115b4f6afc04cd6
VoperAD <victoraguiarrodrigues@yahoo.com>
2024-04-25 21:37:15 +0200

    refactor: return full product info on wishlist endpoints

--- Back/app/api/routers/wish_list.py
+++ Back/app/api/routers/wish_list.py
@@ -1,53 +1,65 @@
 from fastapi import APIRouter
 
 from app.api.deps import CurrentUserDep, WishListServiceDep
-from app.schemas.users.in_wish_list import InWishList, InWishListCreate
+from app.schemas.users.in_wish_list import (
+    InWishListCreate,
+    CompleteWishList,
+)
 
 list_token_router = APIRouter(prefix="/wish_list/me", tags=["Wish Lists"])
 
 
-@list_token_router.get("/", response_model=list[InWishList])
+@list_token_router.get(
+    "/",
+    response_model=list[CompleteWishList],
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+)
 async def read_list_items(
-    *, user_dep: CurrentUserDep, wish_list_service: WishListServiceDep
+    *, current_user: CurrentUserDep, wish_list_service: WishListServiceDep
 ):
     """
-    Retrieve list items from buyer.
+    Retrieve all wishlist items from the current user.
     """
-    wish_list_items = wish_list_service.get_by_id_buyer(id_buyer=user_dep.id)
-    return wish_list_items
+    return wish_list_service.get_all_by_user(current_user)
 
 
-@list_token_router.post("/", response_model=InWishList)
+@list_token_router.post(
+    "/",
+    response_model=CompleteWishList,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+)
 async def create_list_item(
     *,
-    user_dep: CurrentUserDep,
+    current_user: CurrentUserDep,
     wish_list_item: InWishListCreate,
     wish_list_service: WishListServiceDep,
 ):
     """
-    Create a new wish list item for the buyer.
+    Create a new wishlist item for the current user.
     """
-    return wish_list_service.add(id_buyer=user_dep.id, wish_list_item=wish_list_item)
+    return wish_list_service.add_by_user(current_user, wish_list_item)
 
 
 @list_token_router.delete("/")
-async def delete_list(wish_list_service: WishListServiceDep, user_dep: CurrentUserDep):
+async def delete_list(
+    current_user: CurrentUserDep, wish_list_service: WishListServiceDep
+):
     """
-    Delete all list items from a buyer.
+    Delete all wishlist items from the current user.
     """
-    return wish_list_service.delete_by_id_buyer(id_buyer=user_dep)
+    return wish_list_service.delete_all_by_user(current_user)
 
 
 @list_token_router.delete("/{seller_product_id}")
 async def delete_list_item(
     *,
+    current_user: CurrentUserDep,
     seller_product_id: int,
     wish_list_service: WishListServiceDep,
-    user_dep: CurrentUserDep,
 ):
     """
-    Delete an item from a wish list.
+    Delete an item from the current user's wish list.
     """
-    return wish_list_service.delete_by_id(
-        id_buyer=int(user_dep.id), id_seller_product=int(seller_product_id)
-    )
+    return wish_list_service.delete_one_by_user(current_user, seller_product_id)
