afb582e52346acb086ccd829cd566d48b4e16b01
JuanArano17 <juanarano17@gmail.com>
2024-04-26 11:57:52 +0200

    ShoppingCartRefactor

--- Front/src/pages/ShoppingCartPage.jsx
+++ Front/src/pages/ShoppingCartPage.jsx
@@ -7,34 +7,28 @@ import CartItem from '../components/products/shoppingcart/CartItem';
 import CartTotal from '../components/products/shoppingcart/CartTotal';
 import styles from '../styles/styles';
 import { getCartItems } from '../api/services/products/ShoppingCartService';
-import { getAllProducts } from '../api/services/products/ProductsService';
 
 const ShoppingCartPage = () => {
     const [cartItems, setCartItems] = useState([]);
-
+    const fetchCartData = async () => {
+        try {
+            const cartItems = await getCartItems();
+            console.log("Items recibidos del backend:", cartItems);
+            cartItems.sort((a, b) => a.seller_product.id - b.seller_product.id);
+            setCartItems(cartItems);
+        } catch (error) {
+            console.error('Error al cargar los datos del carrito:', error);
+        }
+    };
     useEffect(() => {
-        const fetchCartData = async () => {
-            try {
-                const cartItems = await getCartItems();
-                const allProducts = await getAllProducts();
-
-                const itemsWithDetails = cartItems.map(cartItem => {
-                    const product = allProducts.find(p => p.id_seller_product === cartItem.id_seller_product);
-                    return { ...cartItem, ...product };
-                });
-
-                console.log("Items recibidos del backend:", itemsWithDetails);
-                setCartItems(itemsWithDetails);
-            } catch (error) {
-                console.error('Error al cargar los datos del carrito:', error);
-            }
-        };
-
         fetchCartData();
     }, []);
 
-    const calculateTotal = () => cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
-
+    const calculateTotal = () => {
+        return cartItems.reduce((acc, item) => {
+            return acc + (item.seller_product.price + item.seller_product.shipping_costs) * item.quantity;
+        }, 0);
+    };
     const history = useHistory();
 
     useEffect(() => {
@@ -47,12 +41,12 @@ const ShoppingCartPage = () => {
         <Box sx={styles.mainBox}>
             <TopBar showLogoutButton={true} />
             <Container component="main" maxWidth="md" sx={styles.mainContainer}>
-                <Typography variant="h4" sx={{color:'#629C44', my: 2, fontWeight:'bold' }}>
+                <Typography variant="h4" sx={{ color: '#629C44', my: 2, fontWeight: 'bold' }}>
                     Shopping Cart
                 </Typography>
                 <Grid container spacing={2}>
                     {cartItems.map((item) => (
-                        <CartItem key={item.id_seller_product} item={item} setCartItems={setCartItems} />
+                        <CartItem key={item.seller_product.id} item={item.seller_product} quantity={item.quantity} setCartItems={fetchCartData} />
                     ))}
                     <Grid item xs={12}>
                         <CartTotal total={calculateTotal()} />
