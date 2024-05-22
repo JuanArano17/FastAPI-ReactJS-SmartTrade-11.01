0d5de7261e95ef2405f56c632655fe2e9e4e84e7
RuboTT2 <rubo_t.t@hotmail.com>
2024-05-22 12:51:39 +0200

    Visual Changes buyingproccess

--- Front/src/pages/BuyingProccessPage.jsx
+++ Front/src/pages/BuyingProccessPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Container, Typography, Grid, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { Box, Container, Typography, Paper, Grid, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import TopBar from '../components/topbar/TopBar';
 import Footer from '../components/footer/Footer';
@@ -13,8 +13,8 @@ const BuyingProcessPage = () => {
     const [addresses, setAddresses] = useState([]);
     const [selectedCard, setSelectedCard] = useState(null);
     const [selectedAddress, setSelectedAddress] = useState(null);
-    const [openAddCard, setOpenAddCard] = useState(false);
-    const [openAddAddress, setOpenAddAddress] = useState(false);
+    const [showAddCardForm, setShowAddCardForm] = useState(false);
+    const [showAddAddressForm, setShowAddAddressForm] = useState(false);
     const history = useHistory();
 
     useEffect(() => {
@@ -33,7 +33,7 @@ const BuyingProcessPage = () => {
         if (selectedCard && selectedAddress) {
             history.push('/buy-summary', { selectedCard, selectedAddress });
         }
-        
+
         console.log("selectedCard: ", selectedCard);
         console.log("selectedAddress:", selectedAddress);
     };
@@ -41,86 +41,88 @@ const BuyingProcessPage = () => {
     const handleAddCard = async (newCard) => {
         const createdCard = await createCard(newCard);
         setCards(prevCards => [...prevCards, createdCard]);
-        setOpenAddCard(false);
+        setShowAddCardForm(false);
     };
 
     const handleAddAddress = async (newAddress) => {
         const createdAddress = await createAddress(newAddress);
         setAddresses(prevAddresses => [...prevAddresses, createdAddress]);
-        setOpenAddAddress(false);
+        setShowAddAddressForm(false);
     };
 
     return (
         <Box sx={styles.mainBox}>
             <TopBar showLogoutButton={true} />
             <Container component="main" maxWidth="md" sx={styles.mainContainer}>
-                <Typography variant="h4" sx={{ color: '#629C44', my: 2, fontWeight: 'bold' }}>
-                    Buying Process
-                </Typography>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <FormControl component="fieldset">
-                            <FormLabel component="legend">Select a Card</FormLabel>
-                            <RadioGroup value={selectedCard} onChange={(e) => setSelectedCard(e.target.value)}>
-                                {cards.map((card) => (
-                                    <FormControlLabel key={card.id} value={card.card_name} control={<Radio />} label={`${card.card_name}`} />
-                                ))}
-                            </RadioGroup>
-                            <Button variant="outlined" sx={{ mt: 2 }} onClick={() => setOpenAddCard(true)}>
-                                Add New Card
-                            </Button>
-                        </FormControl>
+                <Paper sx={styles.paperContainer}>
+                    <Typography variant="h4" sx={{ color: '#629C44', my: 2, fontWeight: 'bold' }}>
+                        Buying Process
+                    </Typography>
+                    <Grid container spacing={2}>
+                        {!showAddCardForm && !showAddAddressForm && (
+                            <>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} md={6}>
+                                        <FormControl component="fieldset">
+                                            <FormLabel component="legend">Select a Card</FormLabel>
+                                            <RadioGroup value={selectedCard} onChange={(e) => setSelectedCard(e.target.value)}>
+                                                {cards.map((card) => (
+                                                    <FormControlLabel key={card.id} value={card.card_name} control={<Radio />} label={`${card.card_name}`} />
+                                                ))}
+                                            </RadioGroup>
+                                            <Button  sx={styles.greenRoundedButton} onClick={() => setShowAddCardForm(true)}>
+                                                Add New Card
+                                            </Button>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <FormControl component="fieldset">
+                                            <FormLabel component="legend">Select a Shipping Address</FormLabel>
+                                            <RadioGroup value={selectedAddress} onChange={(e) => setSelectedAddress(e.target.value)}>
+                                                {addresses.map((address) => (
+                                                    <FormControlLabel key={address.id} value={JSON.stringify(address)} control={<Radio />} label={`${address.street}, ${address.city}`} />
+                                                ))}
+                                            </RadioGroup>
+                                            <Button  sx={styles.greenRoundedButton} onClick={() => setShowAddAddressForm(true)}>
+                                                Add New Address
+                                            </Button>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        sx={{ ...styles.greenRoundedButton, mt: 2 }}
+                                        onClick={handleContinue}
+                                        disabled={!selectedCard || !selectedAddress}
+                                    >
+                                        Continue
+                                    </Button>
+                                </Grid>
+                            </>
+                        )}
+                        {showAddCardForm && (
+                            <Grid item xs={12}>
+                                <AddCard onSave={handleAddCard} />
+                                <Button variant="outlined" sx={{ mt: 2 }} onClick={() => setShowAddCardForm(false)}>
+                                    Cancel
+                                </Button>
+                            </Grid>
+                        )}
+                        {showAddAddressForm && (
+                            <Grid item xs={12}>
+                                <AddAddressForm onAddressCreated={handleAddAddress} />
+                                <Button variant="outlined" sx={{ mt: 2 }} onClick={() => setShowAddAddressForm(false)}>
+                                    Cancel
+                                </Button>
+                            </Grid>
+                        )}
                     </Grid>
-                    <Grid item xs={12}>
-                        <FormControl component="fieldset">
-                            <FormLabel component="legend">Select a Shipping Address</FormLabel>
-                            <RadioGroup value={selectedAddress} onChange={(e) => setSelectedAddress(e.target.value)}>
-                                {addresses.map((address) => (
-                                    <FormControlLabel key={address.id} value={address.street} control={<Radio />} label={`${address.street}, ${address.city}`} />
-                                ))}
-                            </RadioGroup>
-                            <Button variant="outlined" sx={{ mt: 2 }} onClick={() => setOpenAddAddress(true)}>
-                                Add New Address
-                            </Button>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            sx={{ ...styles.greenRoundedButton, mt: 2 }}
-                            onClick={handleContinue}
-                            disabled={!selectedCard || !selectedAddress}
-                        >
-                            Continue
-                        </Button>
-                    </Grid>
-                </Grid>
+                </Paper>
             </Container>
             <Footer />
-            <Dialog open={openAddCard} onClose={() => setOpenAddCard(false)}>
-                <DialogTitle>Add New Card</DialogTitle>
-                <DialogContent>
-                    <AddCard onSave={handleAddCard} />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setOpenAddCard(false)} color="primary">
-                        Cancel
-                    </Button>
-                </DialogActions>
-            </Dialog>
-            <Dialog open={openAddAddress} onClose={() => setOpenAddAddress(false)}>
-                <DialogTitle>Add New Address</DialogTitle>
-                <DialogContent>
-                    <AddAddressForm onAddressCreated={handleAddAddress} />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setOpenAddAddress(false)} color="primary">
-                        Cancel
-                    </Button>
-                </DialogActions>
-            </Dialog>
         </Box>
     );
 };
