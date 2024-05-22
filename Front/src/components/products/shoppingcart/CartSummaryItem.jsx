ad82984b1ba6c8f15520dfa4381810760af1f2d1
RuboTT2 <rubo_t.t@hotmail.com>
2024-05-22 13:36:37 +0200

    visualchanges

--- Front/src/components/products/shoppingcart/CartSummaryItem.jsx
+++ Front/src/components/products/shoppingcart/CartSummaryItem.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
+import { Card, CardContent, Typography, Box } from '@mui/material';
 
 const CartSummaryItem = ({ item }) => {
     const { seller_product: product } = item;
+
+    const calculateSubtotal = () => {
+        const subtotal = (product.price + product.shipping_costs) * item.quantity;
+        return subtotal.toFixed(2);
+    };
 
     return (
         <Card sx={{ display: 'flex', alignItems: 'center', mb: 2, p: 2 }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '20%' }}>
-                    <img src="https://via.placeholder.com/100" alt="Product image" />
+                    <img src={item.seller_product.images} alt="Product image" style={{ maxWidth: '100%', height: 'auto' }} />
                 </Box>
                 <Box sx={{ display: 'flex', flexDirection: 'column', width: '60%' }}>
                     <CardContent sx={{ flex: '1 0 auto' }}>
@@ -28,7 +33,7 @@ const CartSummaryItem = ({ item }) => {
                         Quantity: {item.quantity}
                     </Typography>
                     <Typography variant="subtitle1" component="div">
-                        Subtotal: ${(product.price + product.shipping_costs) * item.quantity}
+                        Subtotal: ${calculateSubtotal()}
                     </Typography>
                 </Box>
             </Box>
