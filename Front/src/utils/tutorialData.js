c57154dcfce11a0b6bac5e7fda2d0825843148e6
JuanArano17 <juanarano17@gmail.com>
2024-05-25 22:54:10 +0200

    language fixes

--- Front/src/utils/tutorialData.js
+++ Front/src/utils/tutorialData.js
@@ -1,82 +1,82 @@
 const tutorialData = [
     {
         title: 'Catalog Page',
-        description: 'La página del catálogo es el corazón de nuestra aplicación, donde los usuarios pueden navegar por todos los productos disponibles. Aquí, los usuarios pueden ver una amplia gama de productos, organizados en una cuadrícula visualmente atractiva. Cada producto muestra información clave como el nombre, la descripción breve, los EcoPoints y el precio. Además, los usuarios pueden filtrar los productos por categoría y rango de precios utilizando los controles en la parte superior de la página.',
+        description: 'The catalog page is the heart of our application, where users can browse all available products. Here, users can see a wide range of products, organized in a visually appealing grid. Each product displays key information such as name, brief description, EcoPoints, and price. Additionally, users can filter products by category and price range using the controls at the top of the page.',
         image: process.env.PUBLIC_URL + '/images/tutorial/Catalog.png'
     },
     {
         title: 'Filter Products',
-        description: 'En la página del catálogo, los usuarios pueden aplicar filtros para refinar su búsqueda. El filtro de categoría permite seleccionar diferentes tipos de productos, como electrónicos, ropa, libros, etc. El control deslizante de rango de precios permite a los usuarios establecer un rango de precios específico para encontrar productos que se ajusten a su presupuesto. También hay un botón para limpiar todos los filtros aplicados y reiniciar la búsqueda.',
+        description: 'On the catalog page, users can apply filters to refine their search. The category filter allows selecting different types of products, such as electronics, clothing, books, etc. The price range slider allows users to set a specific price range to find products that fit their budget. There is also a button to clear all applied filters and reset the search.',
         image: process.env.PUBLIC_URL + '/images/tutorial/FilterProducts.png'
     },
     {
         title: 'Home Page',
-        description: 'La página de inicio da la bienvenida a los usuarios con una presentación general de nuestra aplicación. Aquí, los usuarios pueden obtener una visión rápida de lo que ofrece Smart Trade, incluyendo su compromiso con las ventas ecológicas. La página presenta un diseño limpio y atractivo con un mensaje de bienvenida y una imagen representativa que resalta nuestra misión.',
+        description: 'The home page welcomes users with an overview of our application. Here, users can get a quick glimpse of what Smart Trade offers, including its commitment to green sales. The page features a clean and attractive design with a welcome message and a representative image highlighting our mission.',
         image: process.env.PUBLIC_URL + '/images/tutorial/HomePage.png'
     },
     {
         title: 'Login Page',
-        description: 'La página de inicio de sesión permite a los usuarios acceder a sus cuentas personales en Smart Trade. Los usuarios deben ingresar su correo electrónico y contraseña para autenticarse. También se proporciona un enlace para recuperar la contraseña en caso de que los usuarios la olviden. Esta página es esencial para proteger la información del usuario y ofrecer una experiencia personalizada.',
+        description: 'The login page allows users to access their personal accounts on Smart Trade. Users need to enter their email and password to authenticate. A link to recover the password is also provided in case users forget it. This page is essential to protect user information and offer a personalized experience.',
         image: process.env.PUBLIC_URL + '/images/tutorial/Login.png'
     },
     {
         title: 'Login General Page',
-        description: 'La página de inicio de sesión general permite a los usuarios registrarse o iniciar sesión en Smart Trade. Desde aquí, los usuarios pueden elegir entre registrarse como nuevos usuarios o iniciar sesión si ya tienen una cuenta. Este punto de entrada es crucial para garantizar que solo los usuarios autorizados puedan acceder a las funcionalidades avanzadas de la aplicación.',
+        description: 'The general login page allows users to register or log in to Smart Trade. From here, users can choose to sign up as new users or log in if they already have an account. This entry point is crucial to ensure that only authorized users can access the advanced functionalities of the application.',
         image: process.env.PUBLIC_URL + '/images/tutorial/LoginGeneral.png'
     },
     {
         title: 'Products Panel Seller',
-        description: 'El panel de productos para vendedores permite a los usuarios que venden productos en Smart Trade gestionar su inventario. En esta página, los vendedores pueden ver una lista de todos sus productos, junto con detalles como la cantidad disponible, el precio, los costos de envío y el estado de publicación. También pueden realizar acciones como editar o eliminar productos directamente desde esta interfaz.',
+        description: 'The products panel for sellers allows users who sell products on Smart Trade to manage their inventory. On this page, sellers can see a list of all their products, along with details such as available quantity, price, shipping costs, and publication status. They can also perform actions such as editing or deleting products directly from this interface.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProductsPanelSeller.png'
     },
     {
         title: 'Add Product Panel',
-        description: 'El formulario para añadir productos permite a los vendedores agregar nuevos productos a su catálogo. Los vendedores deben proporcionar detalles como el nombre del producto, la categoría, la descripción, la hoja de especificaciones, el precio, los costos de envío y la cantidad disponible. También pueden subir imágenes del producto para atraer a los compradores. Este formulario asegura que los productos se presenten de manera completa y atractiva en la tienda.',
+        description: 'The add product form allows sellers to add new products to their catalog. Sellers need to provide details such as product name, category, description, specification sheet, price, shipping costs, and available quantity. They can also upload product images to attract buyers. This form ensures that products are presented comprehensively and attractively in the store.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProductsPanelSellerAdd.png'
     },
     {
         title: 'Profile Page',
-        description: 'La página de perfil del usuario muestra información personal y permite editarla. Los usuarios pueden ver detalles como su nombre, apellido, correo electrónico, CIF y datos bancarios. Esta página es esencial para que los usuarios mantengan su información actualizada y gestionen sus datos personales de manera segura.',
+        description: 'The user profile page displays personal information and allows editing. Users can view details such as their first name, last name, email, tax ID, and bank details. This page is essential for users to keep their information updated and manage their personal data securely.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProfilePageSeller.png'
     },
     {
         title: 'Profile Page - Buyer Cards',
-        description: 'En la sección de tarjetas de la página de perfil, los usuarios pueden gestionar sus métodos de pago. Pueden añadir, editar o eliminar tarjetas de crédito y débito para facilitar sus compras en Smart Trade. Esta funcionalidad asegura que los usuarios tengan siempre un método de pago actualizado y válido.',
+        description: 'In the cards section of the profile page, users can manage their payment methods. They can add, edit, or delete credit and debit cards to facilitate their purchases on Smart Trade. This functionality ensures that users always have an updated and valid payment method.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProfilePageBuyerCards.png'
     },
     {
         title: 'Profile Page - Buyer Personal Info',
-        description: 'La sección de información personal de la página de perfil permite a los usuarios ver y actualizar sus datos personales. Esto incluye detalles como nombre, apellido, dirección de correo electrónico, DNI, fecha de nacimiento y dirección de facturación. Mantener esta información actualizada es crucial para una comunicación efectiva y una experiencia personalizada.',
+        description: 'The personal information section of the profile page allows users to view and update their personal details. This includes details such as first name, last name, email address, tax ID, date of birth, and billing address. Keeping this information updated is crucial for effective communication and a personalized experience.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProgilePageBuyerPersonalInfo.png'
     },
     {
         title: 'Profile Page - Buyer Shipping Address',
-        description: 'En la sección de direcciones de envío de la página de perfil, los usuarios pueden gestionar sus direcciones de envío. Pueden añadir nuevas direcciones, editar las existentes o eliminar las que ya no necesiten. Esto garantiza que los productos comprados se envíen a la dirección correcta.',
+        description: 'In the shipping address section of the profile page, users can manage their shipping addresses. They can add new addresses, edit existing ones, or delete those no longer needed. This ensures that purchased products are shipped to the correct address.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ProgilePageBuyerShippingAddress.png'
     },
     {
         title: 'Register Buyer',
-        description: 'La página de registro de compradores permite a los nuevos usuarios crear una cuenta en Smart Trade. Los usuarios deben proporcionar información básica como nombre, apellido, dirección de correo electrónico, DNI y una contraseña. También tienen la opción de añadir información de la tarjeta de crédito para facilitar futuras compras.',
+        description: 'The buyer registration page allows new users to create an account on Smart Trade. Users need to provide basic information such as first name, last name, email address, tax ID, and a password. They also have the option to add credit card information to facilitate future purchases.',
         image: process.env.PUBLIC_URL + '/images/tutorial/RegisterBuyer.png'
     },
     {
         title: 'Register Seller',
-        description: 'La página de registro de vendedores permite a los usuarios registrarse como vendedores en Smart Trade. Deben proporcionar información básica como nombre, apellido, dirección de correo electrónico y una contraseña, además de detalles adicionales como el CIF y los datos bancarios para recibir pagos.',
+        description: 'The seller registration page allows users to sign up as sellers on Smart Trade. They need to provide basic information such as first name, last name, email address, and a password, in addition to additional details such as the tax ID and bank details to receive payments.',
         image: process.env.PUBLIC_URL + '/images/tutorial/RegisterSeller.png'
     },
     {
         title: 'Search Bar',
-        description: 'La barra de búsqueda permite a los usuarios buscar productos específicos en Smart Trade. Al ingresar palabras clave en el campo de búsqueda, los usuarios pueden encontrar rápidamente los productos que desean. Esta funcionalidad es esencial para mejorar la experiencia de usuario y facilitar la navegación en la tienda.',
+        description: 'The search bar allows users to search for specific products on Smart Trade. By entering keywords into the search field, users can quickly find the products they want. This functionality is essential to improve user experience and facilitate navigation in the store.',
         image: process.env.PUBLIC_URL + '/images/tutorial/SearchBar.png'
     },
     {
         title: 'Shopping Cart',
-        description: 'El carrito de compras permite a los usuarios ver y gestionar los productos que desean comprar. Pueden ajustar las cantidades, eliminar productos y ver el total acumulado. Esta funcionalidad es crucial para proporcionar una experiencia de compra conveniente y eficiente.',
+        description: 'The shopping cart allows users to view and manage the products they want to buy. They can adjust quantities, remove products, and see the accumulated total. This functionality is crucial to provide a convenient and efficient shopping experience.',
         image: process.env.PUBLIC_URL + '/images/tutorial/ShoppingCart.png'
     },
     {
         title: 'Wish List',
-        description: 'La lista de deseos permite a los usuarios guardar productos que les interesan para comprarlos más tarde. Los usuarios pueden añadir o eliminar productos de su lista de deseos y ver los precios actuales. Esta funcionalidad ayuda a los usuarios a recordar productos de interés y planificar sus compras futuras.',
+        description: 'The wish list allows users to save products they are interested in for later purchase. Users can add or remove products from their wish list and see current prices. This functionality helps users remember products of interest and plan their future purchases.',
         image: process.env.PUBLIC_URL + '/images/tutorial/WishList.png'
     }
 ];
