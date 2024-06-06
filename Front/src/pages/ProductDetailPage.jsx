7c7948f07bbc0438d5eb998e08ae4336c0086a69
Rubo0000 <rubenarranz@hotmail.com>
2024-06-06 12:31:16 +0200

    Slider for similarproducts

--- Front/src/pages/ProductDetailPage.jsx
+++ Front/src/pages/ProductDetailPage.jsx
@@ -11,6 +11,9 @@ import FavoriteButton from '../components/favorite-button/FavoriteButton';
 import ImageSelector from '../components/products/ImageSelector/ImageSelector';
 import SimilarProduct from '../components/products/similarProduct/SimilarProduct';
 import ProductReviews from '../components/reviews/ProductReviews'; 
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductDetailPage = () => {
     const { id } = useParams();
@@ -131,6 +134,32 @@ const ProductDetailPage = () => {
         return <Typography color="error">{error}</Typography>;
     }
 
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
         <Box sx={styles.mainBox}>
             <TopBar showSearchBar={true} showLogoutButton={true} />
@@ -182,13 +211,11 @@ const ProductDetailPage = () => {
                             <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
                                 Similar Products
                             </Typography>
-                            <Grid container spacing={2}>
+                            <Slider {...sliderSettings}>
                                 {similarProducts.map((product) => (
-                                    <Grid item key={product.id} xs={12} sm={6} md={4}>
-                                        <SimilarProduct product={product} />
-                                    </Grid>
+                                    <SimilarProduct key={product.id} product={product} />
                                 ))}
-                            </Grid>
+                            </Slider>
                         </Box>
                         <Divider sx={styles.ThickDivider}></Divider>
                         <ProductReviews productId={id} />  {/* Añade el nuevo componente de reseñas */}
